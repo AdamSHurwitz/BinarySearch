a8ca5bb15a2aed8623d1e255d2e137f4cf0b15b7
adamhurwitz <adamscotthurwitz@gmail.com>
2016-02-19 16:41:59 -0800

    Logged # of Rounds to solve.

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -11,31 +11,35 @@ public class Foo {
     static int arrayLength;
     static int indexGuess;
     static int timesSearched;
+    static int round;
 
     public static void main(String[] arg) {
         int target = 73;
         int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
                 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-        System.out.println("Answer: " + binarySearch(primes, target));
+        System.out.println("Answer is Index: " + binarySearch(primes, target));
     }
 
     public static int binarySearch(int[] primes, int target) {
         arrayLength = primes.length;
         max = arrayLength - 1;
         System.out.println("Begin: min - " + min + " | max - " + max + "\n");
-        while (indexFound == -1){
+        while (indexFound == -1) {
+            round += 1;
             indexGuess = (max + min) / 2;
             if (primes[indexGuess] == target) {
                 indexFound = indexGuess;
-                System.out.println("indexFound - " + indexFound);
+                System.out.println("Round (" + round + ") indexFound - " + indexFound);
                 //run = false;
             } else if (primes[indexGuess] < target) {
                 min = indexGuess + 1;
-                System.out.println("indexGuess - " + indexGuess + " | min - " + min +
+                System.out.println("Round (" + round + ") indexGuess - " + indexGuess
+                        + " | min - " + min +
                         " | max - " + max);
             } else if (primes[indexGuess] > target) {
                 max = indexGuess - 1;
-                System.out.println("indexGuess - " + indexGuess + " | min - " + min +
+                System.out.println("Round (" + round + ") indexGuess - " + indexGuess
+                        + " | min - " + min +
                         " | max - " + max);
             } else {
                 System.out.println("indexFound - not found");
