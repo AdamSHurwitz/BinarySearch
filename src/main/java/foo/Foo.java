46c72a034b697293eeb1f03923135a6e11d3fc0f
adamhurwitz <adamscotthurwitz@gmail.com>
2016-02-19 16:35:05 -0800

    solved

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -13,12 +13,6 @@ public class Foo {
     static int timesSearched;
 
     public static void main(String[] arg) {
-        //1. get indices length
-        //2. set min and max
-        //3. assess value at avg of min + max (round down)
-        //4. if match, return that index, if too low, set 1+ num as min
-        //if too high, set num - 1 as max index
-        //5.
         int target = 73;
         int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
                 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
